3c5a2b5c49a55aeb0bb3ce7ba294d1819d997f69
awitgaming <156307787+awitgaming@users.noreply.github.com>
2025-08-26 15:11:33 +0800

    Step 2: Set up the tools  i need in QueuingForm

--- QueuingForm/Form1.Designer.cs
+++ QueuingForm/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace QueuingForm
 {
-    partial class Form1
+    partial class QueuingForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCashier = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblQueue = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnCashier
+            // 
+            this.btnCashier.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnCashier.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnCashier.Location = new System.Drawing.Point(34, 54);
+            this.btnCashier.Name = "btnCashier";
+            this.btnCashier.Size = new System.Drawing.Size(170, 157);
+            this.btnCashier.TabIndex = 0;
+            this.btnCashier.Text = "Cashier";
+            this.btnCashier.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(30, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "*Click to get a number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.5F);
+            this.label2.Location = new System.Drawing.Point(279, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(247, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = " Position in Queue";
+            // 
+            // lblQueue
+            // 
+            this.lblQueue.AutoSize = true;
+            this.lblQueue.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F);
+            this.lblQueue.Location = new System.Drawing.Point(241, 112);
+            this.lblQueue.Name = "lblQueue";
+            this.lblQueue.Size = new System.Drawing.Size(152, 85);
+            this.lblQueue.TabIndex = 3;
+            this.lblQueue.Text = "P - ";
+            // 
+            // QueuingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(650, 285);
+            this.Controls.Add(this.lblQueue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCashier);
+            this.Name = "QueuingForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCashier;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblQueue;
     }
 }
 
--- QueuingForm/Program.cs
+++ QueuingForm/Program.cs
@@ -16,7 +16,7 @@ namespace QueuingForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new QueuingForm());
         }
     }
 }
