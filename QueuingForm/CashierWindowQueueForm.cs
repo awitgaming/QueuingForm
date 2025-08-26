181ddb50bce7bfb54fa7b084fcd40657f2d46244
awitgaming <156307787+awitgaming@users.noreply.github.com>
2025-08-26 16:07:30 +0800

    Step 9 & 10: call the event, add method and follow the code in instruction

--- QueuingForm/CashierWindowQueueForm.cs
+++ QueuingForm/CashierWindowQueueForm.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,6 +21,22 @@ namespace QueuingForm
 
         private void CashierWindowQueueForm_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+        }
+
+        public void DisplayCashierQueue(IEnumerable CashierList)
+        {
+
+            listCashierQueue.Items.Clear();
+            foreach (Object obj in CashierList)
+            {
+                listCashierQueue.Items.Add(obj.ToString());
+            }
 
         }
     }
