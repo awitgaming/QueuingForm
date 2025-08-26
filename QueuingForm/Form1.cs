e02d2a12a1bd45b8364805db0f3593052903f281
awitgaming <156307787+awitgaming@users.noreply.github.com>
2025-08-26 15:34:33 +0800

    Step 4 & 5: Click the button and call cashierclass and variable name is class, access modi is private

--- QueuingForm/Form1.cs
+++ QueuingForm/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,19 @@ namespace QueuingForm
 {
     public partial class QueuingForm : Form
     {
+        private CashierClass cashier;
         public QueuingForm()
         {
             InitializeComponent();
+            cashier = new CashierClass();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void btnCashier_Click(object sender, EventArgs e)
         {
 
         }
